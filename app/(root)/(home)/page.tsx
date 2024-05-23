b641deda7e3ccbe39c449789a15e8973721187ec
shahid <shahidazhari313@gmail.com>
2024-05-23 22:03:45 +0530

    sidebar ok

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,13 @@
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <div>Home</div>
-   )
- }
- 
- export default Home
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className='text-3xl font-bold'>
+        Home
+      </h1>
+    </section>
+  )
+}
+
+export default Home
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,35 @@
+'use client'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = () => {
+  const pathname = usePathname()
+          
+
   return (
-    <div>Sidebar</div>
+    <section className='sticky p-6 pt-28 max-sm:hidden lg:w-[264px] text-white left-0 top-0 flex h-screen flex-col justify-between bg-dark-1 w-fit'>
+      <div className="flex flex-1 flex-col gap-6">
+
+        {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route || pathname.startsWith(link.route);
+          
+
+          return (
+            <Link href={link.route} key={link.label}
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', { 'bg-blue-1': isActive, })}>
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {link.label}</p>
+            </Link>
+          )
+        })}
+
+      </div>
+    </section>
   )
 }
 
